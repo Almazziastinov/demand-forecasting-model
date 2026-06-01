85ca249fe01762a2b8f6e7c975ef91cb262ab123
Almaz Alziastinov <ziastinovalmaz@gmail.com>
2026-06-01 16:49:09 +0300

    fix: use current check lines export source

--- scripts/clickhouse_export_template.sql
+++ scripts/clickhouse_export_template.sql
@@ -22,24 +22,24 @@ The exporter will substitute:
   - {limit_clause}
 */
 SELECT
-    fcln.check_datetime AS check_datetime,
-    fcln.check_date AS check_date,
-    fcln.cash_event_type AS cash_event_type,
-    fcln.quantity AS quantity,
-    fcln.price AS price,
-    fcln.line_amount AS line_amount,
-    fcln.freshness AS freshness,
-    fcln.bakery_id AS bakery_id,
+    fcl.check_datetime AS check_datetime,
+    fcl.check_date AS check_date,
+    fcl.cash_event_type AS cash_event_type,
+    fcl.quantity AS quantity,
+    fcl.price AS price,
+    fcl.line_amount AS line_amount,
+    fcl.freshness AS freshness,
+    fcl.bakery_id AS bakery_id,
     db.bakery_name AS bakery_name,
     db.city AS city,
-    fcln.product_id AS product_id,
+    fcl.product_id AS product_id,
     dp.product_name AS product_name,
     dp.category_name AS category_name
-FROM Svezhar.fct_check_lines_new AS fcln FINAL
-JOIN Svezhar.dim_bakeries AS db
-    ON db.bakery_id = fcln.bakery_id
-JOIN Svezhar.dim_products AS dp
-    ON dp.product_id = fcln.product_id
-WHERE fcln.cash_event_type = 'Продажа'
-  AND fcln.check_date BETWEEN toDate('{date_from}') AND toDate('{date_to}')
+FROM Svezhar.fct_check_lines AS fcl
+ANY LEFT JOIN Svezhar.dim_bakeries AS db
+    ON db.bakery_id = fcl.bakery_id
+ANY LEFT JOIN Svezhar.dim_products AS dp
+    ON dp.product_id = fcl.product_id
+WHERE hex(fcl.cash_event_type) = 'D09FD180D0BED0B4D0B0D0B6D0B0'
+  AND fcl.check_date BETWEEN toDate('{date_from}') AND toDate('{date_to}')
 {limit_clause}
